96346dd24fe012b4afd4323bc86bb6ae93ad1be7
avishaicohen1112 <avishaicohen1112@users.noreply.github.com>
2026-08-20 01:19:46 +0300

    אייקון מהתמונה האמיתית + שינוי שם האפליקציה ל'הסטיקרים של לוין'

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
    ============================================================ */
 'use strict';
 
-const CACHE = 'yehali-stickers-v1';
+const CACHE = 'yehali-stickers-v2';
 
 const FILES = [
   './',
